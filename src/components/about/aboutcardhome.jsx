69f57937d0ed7f6f796ca2f128c745e4b715d5f1
anujdubey88 <anujd1595@gmail.com>
2024-04-10 09:46:16 +0530

    some changes

--- src/components/about/aboutcardhome.jsx
+++ src/components/about/aboutcardhome.jsx
@@ -5,6 +5,17 @@ export default function AboutCardHome() {
   return (
     <div className="about-section">
       {/* Section 1 */}
+      
+      <div className="about-item">
+        <div className="ourmission">
+          <h3>Our Mission</h3>
+          <p>
+          Enable Students from any background , the ability to upskill in latest technologies and land in amazing careers.
+MSME registered startup, we give every student from any diverse starting point to learn latest technology at their own pace and with practical implementation. Thus, creating a powerful online learning ecosystem for everyone and enable the youth to upskill themselves.
+
+          </p>
+        </div>
+      </div>
       <div className="about-recent-content">
         <h2>Recent Stats:</h2>
         <div className='recentdata'>
@@ -14,20 +25,6 @@ export default function AboutCardHome() {
           <p>15+ Mentors</p>
         </div>
       </div>
-      <div className="about-item">
-        <div className="about-item-left">
-          <img src={process.env.PUBLIC_URL + '/images/bg.jpeg'} alt="Image 1" />
-        </div>
-        <div className="about-item-right">
-          <h3>Our Mission</h3>
-          <p>
-          Enable Students from any background , the ability to upskill in latest technologies and land in amazing careers.
-MSME registered startup, we give every student from any diverse starting point to learn latest technology at their own pace and with practical implementation. Thus, creating a powerful online learning ecosystem for everyone and enable the youth to upskill themselves.
-
-          </p>
-        </div>
-      </div>
-
       {/* Section 2 */}
       <div className="about-item about-item-alt">
         <div className="about-item-left">
