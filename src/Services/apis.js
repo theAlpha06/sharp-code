fffae8ae1c747538aaf9cdb86e8635543ab27a10
anujdubey88 <anujd1595@gmail.com>
2024-04-18 12:23:33 +0530

    server api key

--- src/Services/apis.js
+++ src/Services/apis.js
@@ -1,12 +1,12 @@
 
-const BASE_URL='http://localhost:4000/'
+const BASE_URL=''
 
 // AUTH ENDPOINTS
 export const endpoints = {
-  SIGNUP_API: BASE_URL + "api/v2/auth/signup",
-  LOGIN_API: BASE_URL + "api/v2/auth/login",
-  PROFILE_API: BASE_URL+"api/v2/userdetail/userDetail",
-  SUBMISSION_API: BASE_URL+"api/v2/submission",
-  UPDATE_API: BASE_URL+"api/v2/profile/updateProfile",
-  LOGOUT_API: BASE_URL+"api/v2/auth/logout",
+  SIGNUP_API:  "https://server-2nbi.onrender.com/api/v2/auth/signup",
+  LOGIN_API:  "https://server-2nbi.onrender.com/api/v2/auth/login",
+  PROFILE_API:"https://server-2nbi.onrender.com/api/v2/userdetail/userDetail",
+  SUBMISSION_API:"https://server-2nbi.onrender.com/api/v2/submission",
+  UPDATE_API:"https://server-2nbi.onrender.com/api/v2/profile/updateProfile",
+  LOGOUT_API:"https://server-2nbi.onrender.com/api/v2/auth/logout",
 }
