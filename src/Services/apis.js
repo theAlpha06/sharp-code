8b3b02ef9b3204fc99cd798c8ea93563d85fea02
anujdubey88 <anujd1595@gmail.com>
2024-04-21 14:33:46 +0530

    LOGINapi linke is changed

--- src/Services/apis.js
+++ src/Services/apis.js
@@ -3,7 +3,7 @@
 export const endpoints = {
   SIGNUP_API:   "https://server-qcrx.onrender.com/api/v2/auth/signup",
   LOGIN_API:   "https://server-qcrx.onrender.com/api/v2/auth/login",
-  PROFILE_API: "https://server-qcrx.onrender.com/api/v2/userdetail/userDetail",
+  PROFILE_API: "/api/v2/userdetail/userDetail",
   SUBMISSION_API: "https://server-qcrx.onrender.com/api/v2/submission",
   UPDATE_API: "https://server-qcrx.onrender.com/api/v2/profile/updateProfile",
   LOGOUT_API: "https://server-qcrx.onrender.com/api/v2/auth/logout",
